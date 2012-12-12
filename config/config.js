1450125c20b04748c326aa3837b1a629bab3ad3d
Ram Ranganathan <ramr@redhat.com>
2012-12-12 15:52:30 -0800

    Sample hummingbird application running on OpenShift.

--- config/config.js
+++ config/config.js
@@ -1,18 +1,21 @@
 module.exports = config = {
   "name" : "Hummingbird",
 
-  "tracking_port" : 8000,
+  "tracking_port" : 18000,
+
+  "dashboard_address" : process.env.OPENSHIFT_NODEJS_IP,
   "dashboard_port" : 8080,
 
-  "mongo_host" : "localhost",
+  "mongo_host" : process.env.OPENSHIFT_MONGODB_IP,
   "mongo_port" : 27017,
 
-  "udp_address" : "127.0.0.1",
-  "udp_port" : 8000,
+  "udp_address" : process.env.OPENSHIFT_NODEJS_IP,
+  "udp_port" : 18000,
 
   "enable_dashboard" : true,
 
-  "demo_mode": true,
+  /*  "demo_mode": true,  */
+  "demo_mode": false,
 
   "capistrano" : {
     "repository" :       "git://github.com/mnutt/hummingbird.git",
